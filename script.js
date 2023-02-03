7c68b5df8f7dce42f6bbebbe8e055b2c102e660b
27sarrazin <sarrazinr5@gmail.com>
2023-02-03 00:05:40 -0400

    primeiro comit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function abrirMenu(){
+    let nav=document.getElementById("menu-descktop")
+     if(nav.style.display =='none'){
+        nav.style.display='block'
+     }else{
+        nav.style.display='none'
+     }
+    
+}
